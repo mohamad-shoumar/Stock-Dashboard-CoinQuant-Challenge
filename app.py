a72bf68223f57b9d552ac568564fd04050c6aa10
MoeShoumar <mks38@mail.aub.edu>
2023-05-31 22:57:49 +0300

    refactor(app.py): remove unused imports and commented code feat(app.py): add route for index page and database models for Stock, StockPrice, PriceChange, and EMA

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 import requests
 from polygon import RESTClient
@@ -6,29 +6,12 @@ import datetime as dt
 import pandas as pd
 import plotly.graph_objects as go
 from plotly.offline import plot
+import yfinance as yf
 
 app = Flask(__name__)
 
 
-polygon_key = "AWMbFAPJRt11b4FWamJ8lLBNOPgqf8pI"
-client = RESTClient(polygon_key)
-stockTicker = "AAPL"
-from_date = "2023-05-01"
-to_date = "2023-05-03"
-dataRequest = client.get_aggs(stockTicker, 1, "hour", from_date, to_date)
-print (dataRequest)
-# BASE_URL = "https://api.polygon.io/v2"
-# symbol_list = ["AAPL", "GOOGL", "MSFT", "AMZN", "TSLA"]
-# timeframe = "hour"
-# api_url = f"{BASE_URL}/aggs/grouped/locale/us/market/stocks/tickers/{','.join(symbol_list)}/prev?apiKey=AWMbFAPJRt11b4FWamJ8lLBNOPgqf8pI&unadjusted=true&limit=5000&sort=asc&apiKey=AWMbFAPJRt11b4FWamJ8lLBNOPgqf8pI"
-
-
-
-
-
-
-
-
+# Database Models
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///stocks.db'
 db = SQLAlchemy(app)
 
@@ -37,25 +20,23 @@ class Stock(db.Model):
     symbol = db.Column(db.String(10))
     name = db.Column(db.String(100))
     marketcap = db.Column(db.Float)
-
     price_changes = db.relationship('PriceChange', backref='stock', cascade='all, delete-orphan')
     stock_prices = db.relationship('StockPrice', backref='stock', cascade='all, delete-orphan')
     emas = db.relationship('EMA', backref='stock', cascade='all, delete-orphan')
-
-class PriceChange(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    stock_id = db.Column(db.Integer, db.ForeignKey('stock.id'))
-    change_percent = db.Column(db.Float)
-    hours_1 = db.Column(db.Float)
-    hours_4 = db.Column(db.Float)
-    hours_12 = db.Column(db.Float)
-    hours_24 = db.Column(db.Float)
-
 class StockPrice(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     timestamp = db.Column(db.DateTime)
     stock_id = db.Column(db.Integer, db.ForeignKey('stock.id'))
     price = db.Column(db.Float)
+class PriceChange(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    stock_id = db.Column(db.Integer, db.ForeignKey('stock.id'))
+    timestamp = db.Column(db.DateTime)
+    hours_1 = db.Column(db.Float)
+    hours_4 = db.Column(db.Float)
+    hours_12 = db.Column(db.Float)
+    hours_24 = db.Column(db.Float)
+
 
 class EMA (db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -64,24 +45,41 @@ class EMA (db.Model):
     hours_4 = db.Column(db.Float)
     hours_12 = db.Column(db.Float)
     hours_24 = db.Column(db.Float)
-    value = db.Column(db.Float)
+    timestamp = db.Column(db.DateTime)
 
-    
 
+# store price difference and prices in DB
+# def store_price_data():
+#     with app.app_context():
+#         last_1_hour_diff = priceData['1_hour_diff'].iloc[-1]
+#         last_4_hours_diff = priceData['4_hours_diff'].iloc[-1]
+#         last_12_hours_diff = priceData['12_hours_diff'].iloc[-1]
+#         last_24_hours_diff = priceData['24_hours_diff'].iloc[-1]
+
+#         stock_price = StockPrice(timestamp=dt.datetime.now(), price=priceData['close'].iloc[-1], stock_id=1)
+#         price_change = PriceChange(
+#             change_percent=last_1_hour_diff,
+#     hours_1=last_1_hour_diff,
+#     hours_4=last_4_hours_diff,
+#     hours_12=last_12_hours_diff,
+#     hours_24=last_24_hours_diff
+#         )
+#         db.session.add(stock_price)
+#         db.session.add(price_change)
+#         db.session.commit()
+            
+
+# route 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    response = requests.get(api_url)
-    if response.status_code == 200:
-        data = response.json()
-        results = data["results"]
-        stock_data = [result['T'] for result in results]
-    else:
-        print("Error occurred while fetching data:", response.status_code)
-        stock_data = []
+    # store_price_data()
+    # stocks = Stock.query.first() 
+    return render_template('index.html')
 
-    return render_template('index.html', stock_data=stock_data)
+
+
 
 if __name__ == '__main__':
     with app.app_context():
-        db.create_all()
-    app.run(debug=True)
+        index()
+        app.run(debug=True)
